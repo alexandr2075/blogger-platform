9b03fbf0a3d5b3eac4d841462a5584e6931c298f
alexandr2075 <gagara5620@gmail.com>
2025-05-30 06:47:02 +0900

    feat(config): добавить динамический модуль конфигурации и тестовые хелперы для комментариев
    - Добавлен динамический модуль конфигурации для управления переменными окружения
    - Реализованы тестовые хелперы для создания комментариев и получения токена доступа
    - Обновлены тестовые сценарии для работы с блогами и комментариями

--- src/config-dynamic-module.ts
+++ src/config-dynamic-module.ts
@@ -3,10 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 // you must import this const in the head of your app.module.ts
 export const configModule = ConfigModule.forRoot({
   envFilePath: [
-    // process.env.ENV_FILE_PATH?.trim() || '',
-    // `.env.${process.env.NODE_ENV}.local`,
-    // `.env.${process.env.NODE_ENV}`,
-    // '.env.production',
+    process.env.ENV_FILE_PATH?.trim() || '',
+    `.env.${process.env.NODE_ENV}.local`,
+    `.env.${process.env.NODE_ENV}`,
+    '.env.production',
     '.env',
   ],
   isGlobal: true,
